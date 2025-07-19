5b97e07d5e05e016a29908cd99b88cafca8774a5
ahMADASSadi <ds1380ahmad@gmail.com>
2025-07-19 16:46:47 +0330

    chore: setup database

---
+++ internal/db/database.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"log"
+
+	"github.com/ahMADASSadi/goTravel/internal/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase(dbDriver, dbSource string) {
+	var err error
+	if dbDriver == "sqlite3" {
+		DB, err = gorm.Open(sqlite.Open(dbSource), &gorm.Config{})
+	}
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	log.Println("Database connected")
+
+	migrate()
+}
+
+func migrate() {
+	// Auto-migrate your tables here
+	err := DB.AutoMigrate(
+		// &Origin{},
+		// &Target{},
+		// &Terminal{},
+		// &Reservation{},
+		// &Purchase{},
+		&models.WeeklyTimeSchedule{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database: ", err)
+	}
+	log.Println("Database migrated")
+}
