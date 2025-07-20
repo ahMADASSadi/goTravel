2f82b6766912514a79de3b2f12dd3e0ef65563e6
ahMADASSadi <ds1380ahmad@gmail.com>
2025-07-20 16:56:09 +0330

    feat: add custom responses

---
+++ internal/responses/response.go
@@ -0,0 +1,30 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/ahMADASSadi/goTravel/internal/errors"
+	"github.com/gin-gonic/gin"
+)
+
+func Success(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    data,
+	})
+}
+
+func Error(c *gin.Context, err error) {
+	switch e := err.(type) {
+	case *errors.AppError:
+		c.JSON(e.Code, gin.H{
+			"success": false,
+			"error":   e.Message,
+		})
+	default:
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   "unexpected server error",
+		})
+	}
+}
