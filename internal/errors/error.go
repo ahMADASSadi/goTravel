773be4383a896f58454fc9f734b0daa7e85e895d
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-07-21 05:07:51 +0330

    feat: add custome errors

--- internal/errors/error.go
+++ internal/errors/error.go
@@ -2,24 +2,30 @@ package errors
 
 import "net/http"
 
-type AppError struct {
+type ApiError struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
-func (e *AppError) Error() string {
+func (e *ApiError) Error() string {
 	return e.Message
 }
 
-func New(code int, message string) *AppError {
-	return &AppError{Code: code, Message: message}
+func New(code int, message string) *ApiError {
+	return &ApiError{Code: code, Message: message}
 }
 
 // Predefined errors
 var (
-	ErrNotFound         = New(http.StatusNotFound, "resource not found")
-	ErrBadRequest       = New(http.StatusBadRequest, "bad request")
-	ErrServerError      = New(http.StatusInternalServerError, "internal server error")
-	ErrNotCreated       = New(http.StatusInternalServerError, "object not created")
-	ErrSeatsUnavailable = New(http.StatusNoContent, "Seats are not available")
+	// 4xx Client Errors
+	ErrNotFound              = New(http.StatusNotFound, "resource not found")
+	ErrBadRequest            = New(http.StatusBadRequest, "bad request")
+	ErrAlreadyRefunded       = New(http.StatusBadRequest, "Ticket is already refunded")
+	ErrSeatsUnavailable      = New(http.StatusConflict, "requested seats are no longer available")
+	ErrDuplicateSocialCode   = New(http.StatusBadRequest, "ducpicate social code")
+	ErrSeatLessThanPassenger = New(http.StatusBadRequest, "seats are less than the number of passangers")
+	// 5xx Client Errors
+	ErrNotCreated   = New(http.StatusInternalServerError, "object not created")
+	ErrServerError  = New(http.StatusInternalServerError, "internal server error")
+	ErrCreateTicket = New(http.StatusInternalServerError, "ticket not created")
 )
