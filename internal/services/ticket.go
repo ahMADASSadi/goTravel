01c06122010e32391764feb3c3cab884a9053912
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-07-21 05:03:53 +0330

    feat: ticket services for refund and create

---
+++ internal/services/ticket.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ahMADASSadi/goTravel/internal/models"
+	"gorm.io/gorm"
+)
+
+func generateTicketNo() string {
+	return fmt.Sprintf("TKT-%d", time.Now().UnixNano())
+}
+func GetSearchIDFromReservation(db *gorm.DB, reservationID uint) (string, error) {
+	var searchID string
+	if err := db.Model(&models.Reservation{}).
+		Select("search_id").
+		Where("id = ?", reservationID).
+		Scan(&searchID).Error; err != nil {
+		return "", err
+	}
+	return searchID, nil
+}
+
+func CreateTicket(tx *gorm.DB, ticketInput *models.TicketCreateInput) (*[]models.Ticket, error) {
+	reservationID := ticketInput.ReservationID
+
+	searchID, err := GetSearchIDFromReservation(tx, reservationID)
+	if err != nil {
+		return nil, err
+	}
+
+	var tickets []models.Ticket
+	for _, v := range ticketInput.Passengers {
+		ticket := models.Ticket{
+			TicketNo:      generateTicketNo(),
+			ReservationID: reservationID,
+			SearchID:      searchID,
+			Passenger:     v,
+			CreatedAt:     time.Now(),
+		}
+		tickets = append(tickets, ticket)
+	}
+
+	if err := tx.Create(&tickets).Error; err != nil {
+		return nil, err
+	}
+	if err := SetReservationStatus(tx, reservationID, uint(models.Success)); err != nil {
+		return nil, err
+	}
+
+	return &tickets, nil
+}
+
+func RefundTicket(tx *gorm.DB, ticketNo string) error {
+	var ticket models.Ticket
+
+	if err := tx.Where("ticket_no = ?", ticketNo).First(&ticket).Error; err != nil {
+		return err
+	}
+
+	if err := tx.Model(&ticket).Update("deleted_at", time.Now()).Error; err != nil {
+		return err
+	}
+	if err := UnmarkReservedSeats(tx, ticket.ReservationID); err != nil {
+		return err
+	}
+
+	return nil
+}
