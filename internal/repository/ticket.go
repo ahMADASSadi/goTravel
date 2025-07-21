6060672a0afd49d7affea8e70592d6b77d95d1b1
ahMADASSadi <ds1380ahmad@gmail.com>
2025-07-21 13:57:40 +0330

    feat: add the city and ticket repositories

--- internal/repository/ticket.go
+++ internal/repository/ticket.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/ahMADASSadi/goTravel/internal/db"
 	"github.com/ahMADASSadi/goTravel/internal/models"
+	"github.com/ahMADASSadi/goTravel/internal/services"
 )
 
 func GetTicketByNumber(ticketNumber string) (*models.Ticket, error) {
@@ -14,7 +15,26 @@ func GetTicketByNumber(ticketNumber string) (*models.Ticket, error) {
 	return &ticket, nil
 }
 
-// func GetTravelInfoWithSearchID(searchID string) (*models.TravelSearch, error) {
-// 	var travelSearch models.TravelSearch
-// 	travelInfo, _, err := services.DecodeSearchID(searchID)
-// }
+func GetTravelInfoWithSearchID(searchID string) (*models.WeeklyTimeSchedule, error) {
+	var travel models.WeeklyTimeSchedule
+	travelID, _, err := services.DecodeSearchID(searchID)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.DB.
+		Where("id = ?", travelID).
+		Preload("Bus").
+		Preload("Bus.Seats").
+		First(&travel).Error; err != nil {
+		return nil, err
+	}
+	return &travel, nil
+}
+
+func GetReservationInfo(reservationID uint) (*models.Reservation, error) {
+	var reservation models.Reservation
+	if err := db.DB.Where("id = ?", reservationID).First(&reservation).Error; err != nil {
+		return nil, err
+	}
+	return &reservation, nil
+}
